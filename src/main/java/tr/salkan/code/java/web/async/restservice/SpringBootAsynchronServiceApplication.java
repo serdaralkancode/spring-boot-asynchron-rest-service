4fe69f61f3573950eccd943e256bdd05991a4164
Serdar ALKAN <serdaralkan_@hotmail.com>
2020-06-06 19:32:48 +0300

    Initial commit

---
+++ src/main/java/tr/salkan/code/java/web/async/restservice/SpringBootAsynchronServiceApplication.java
@@ -0,0 +1,13 @@
+package tr.salkan.code.java.web.async.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsynchronServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAsynchronServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/salkan/code/java/web/async/restservice/SpringBootAsynchronServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.salkan.code.java.web.async.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAsynchronServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
